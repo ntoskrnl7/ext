f921cc00e739eabab358a4a436bca88a83874696
Jung-Kwang Lee <ntoskrnl7@gmail.com>
2020-08-20 01:46:28 +0900

    Fix the process class and process test code

--- test/process.cpp
+++ test/process.cpp
@@ -1,13 +1,36 @@
 
-#include <gtest/gtest.h>
 #include <ext/process>
+#include <gtest/gtest.h>
 
-TEST(process_test, run_list_cwd_cmd)
-{
+TEST(process_test, run_list_working_directory_cmd) {
 #ifdef _WIN32
-    ext::process process("cmd", { "/c", "dir", "/s", "."});
+  ext::process process("cmd", {"/c", "dir", "."});
 #else
-    ext::process process("ls", { "-al", "."});
+  ext::process process("ls", {"-al", "."});
 #endif
-    process.join();
+  EXPECT_TRUE(process.joinable());
+  process.join();
+}
+
+TEST(process_test,
+     run_list_working_directory_cmd_with_working_directory_set_to_root) {
+#ifdef _WIN32
+  std::string systemDrive;
+  size_t requiredCount = sizeof("C:");
+  systemDrive.resize(requiredCount);
+  errno_t err =
+      getenv_s(&requiredCount, &systemDrive[0], requiredCount, "SystemDrive");
+  if (err == ERANGE) {
+    puts(std::to_string(requiredCount).c_str());
+    systemDrive.resize(requiredCount);
+    err =
+        getenv_s(&requiredCount, &systemDrive[0], requiredCount, "SystemDrive");
+  }
+  EXPECT_EQ(err, 0);
+  ext::process process("cmd", systemDrive.c_str(), {"/c", "dir", "."});
+#else
+  ext::process process("ls", "/", {"-al", "."});
+#endif
+  EXPECT_TRUE(process.joinable());
+  process.join();
 }
