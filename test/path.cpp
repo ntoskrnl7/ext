cb7051ff987e9a95657984e3d3cc5c4688f6f30d
Jung-Kwang Lee <ntoskrnl7@gmail.com>
2020-11-03 21:52:30 +0900

    Add a path class and path test code

---
+++ test/path.cpp
@@ -0,0 +1,32 @@
+#include <ext/path>
+#include <gtest/gtest.h>
+
+TEST(path_test, test) {
+#ifdef _WIN32
+  auto r = ext::path::join("c:", "dir");
+  EXPECT_STREQ(r.c_str(), "c:\\dir");
+#else
+  auto r = ext::path::join("/home", "dir");
+  EXPECT_STREQ(r.c_str(), "/home/dir");
+#endif
+#ifdef _WIN32
+  r = ext::path::join("c:", "dir", "file");
+  EXPECT_STREQ(r.c_str(), "c:\\dir\\file");
+#else
+  r = ext::path::join("/home", "dir", "file");
+  EXPECT_STREQ(r.c_str(), "/home/dir/file");
+#endif
+#ifdef _WIN32
+  std::string path = "d:";
+  r = ext::path::join(path, "dir");
+  EXPECT_STREQ(r.c_str(), "d:\\dir");
+  r = ext::path::join(path, std::string("dir"), std::string("file"));
+  EXPECT_STREQ(r.c_str(), "d:\\dir\\file");
+#else
+  std::string path = "/home";
+  r = ext::path::join(path, "dir");
+  EXPECT_STREQ(r.c_str(), "/home/dir");
+  r = ext::path::join(path, std::string("dir"), std::string("file"));
+  EXPECT_STREQ(r.c_str(), "/home/dir/file");
+#endif
+}
