d8c3cd4906661f2c1987f632b21255445453b2f2
Jung-Kwnag Lee <ntoskrnl7@gmail.com>
2020-11-22 00:42:21 +0900

    Fix a type_traits test code

--- test/type_traits.cpp
+++ test/type_traits.cpp
@@ -1,43 +1,57 @@
-﻿
+﻿#include <ext/type_traits>
 #include <ext/typeinfo>
-#include <ext/type_traits>
 #include <gtest/gtest.h>
 
 class test_callable {
 public:
-  std::string operator()(int, char, std::vector<std::string>) { return ""; }
+  std::string operator()(int, char, std::vector<std::string>) { return "1"; }
 };
 
 class test_callable_2 {
 public:
   void operator()(){};
-  std::string operator()(int, char, std::vector<std::string>) { return ""; }
+  std::string operator()(int, char, std::vector<std::string>) { return "2"; }
 };
 
-TEST(deduce_mem_fn_test, test) {
+#ifdef _EXT_REMOVE_CLASS_
+TEST(type_traits_test, remove_class_test) {
+  std::string (test_callable_2::*ptr)(int, char, std::vector<std::string>) =
+      &test_callable_2::operator();
+
+  EXPECT_STREQ(ext::get_type_name<decltype(ptr)>().c_str(),
+               ext::get_type_name<std::string (test_callable_2::*)(
+                   int, char, std::vector<std::string>)>()
+                   .c_str());
+
+  EXPECT_STREQ(
+      ext::get_type_name<ext::remove_class<decltype(ptr)>::type>().c_str(),
+      ext::get_type_name<std::string(int, char, std::vector<std::string>)>()
+          .c_str());
+}
+#endif //_EXT_REMOVE_CLASS_
+
+#ifdef _EXT_DEDUCE_MEM_FN_
+TEST(type_traits_test, deduce_mem_fn_test) {
   auto ld = []() {};
-  auto ts = ext::get_type_name<ext::deduce_mem_fn<decltype(ld)>::type>();
+  EXPECT_STREQ(
+      ext::get_type_name<void()>().c_str(),
+      ext::get_type_name<ext::deduce_mem_fn<decltype(ld)>::type>().c_str());
 
   auto ld2 = [](int, char, std::vector<std::string>) -> std::string {
-    return "";
+    return "3";
   };
-
-  ts = ext::get_type_name<ext::deduce_mem_fn<decltype(ld2)>::type>();
+  EXPECT_STREQ(
+      ext::get_type_name<std::string(int, char, std::vector<std::string>)>()
+          .c_str(),
+      ext::get_type_name<ext::deduce_mem_fn<decltype(ld2)>::type>().c_str());
 
   test_callable obj;
-  ts = ext::get_type_name<test_callable>();
-
-  ts = ext::get_type_name<ext::deduce_mem_fn<decltype(obj)>::type>();
+  EXPECT_STREQ(
+      ext::get_type_name<std::string(int, char, std::vector<std::string>)>()
+          .c_str(),
+      ext::get_type_name<ext::deduce_mem_fn<decltype(obj)>::type>().c_str());
 
   void (test_callable_2::*ptr)(void) = &test_callable_2::operator();
-  ts = ext::get_type_name<ext::remove_class<decltype(ptr)>::type>();
-
-  std::string (test_callable_2::*ptr2)(int, char, std::vector<std::string>) =
-      &test_callable_2::operator();
-  ts = ext::get_type_name<ext::remove_class<decltype(ptr2)>::type>();
-
-  ts = ext::get_type_name<decltype(ptr2)>();
-
-  test_callable_2 obj2;
-  std::bind(ptr, obj2)();
+  auto ts = ext::get_type_name<ext::remove_class<decltype(ptr)>::type>();
 }
+#endif // _EXT_DEDUCE_MEM_FN_
