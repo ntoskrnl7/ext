afb2d12cb012e94e27a0eb9da68c91418d1189fb
ntoskrnl7 <47030112+ntoskrnl7@users.noreply.github.com>
2021-09-16 08:46:44 +0900

    Update init-msys.sh

--- init-msys.sh
+++ init-msys.sh
@@ -1,4 +1,5 @@
 #!/usr/bash
+
 pacman -Syu
 pacman -Su
 pacman -S --needed base-devel git
@@ -6,7 +7,10 @@ pacman -S --needed base-devel git
 case "${MSYSTEM}" in
     "MINGW32") pacman -S --needed mingw-w64-i686-toolchain mingw-w64-i686-cmake;;
     "MINGW64") pacman -S --needed mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake;;
-    "CLANG32") pacman -S --needed mingw-w64-clang-i686-toolchain mingw-w64-clang-i686-cmake;;
+    "CLANG32")
+        sed -i '1s|^|[staging]\nServer = https://repo.msys2.org/staging/\nSigLevel = Never\n[clang32]\nInclude = /etc/pacman.d/mirrorlist.clang32\n|' /etc/pacman.conf
+        pacman -Syu
+        pacman -S --needed mingw-w64-clang-i686-toolchain mingw-w64-clang-i686-cmake;;
     "CLANG64") pacman -S --needed mingw-w64-clang-x86_64-toolchain mingw-w64-clang-x86_64-cmake;;
     "CLANGARM64") pacman -S --needed mingw-w64-clang-aarch64-toolchain mingw-w64-clang-aarch64-cmake;;
     "UCRT64") pacman -S --needed mingw-w64-ucrt-x86_64-toolchain mingw-w64-ucrt-x86_64-cmake;;
