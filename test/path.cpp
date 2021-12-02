6750aa424967acef0fa75edb5480d202a073994a
Jung-Kwang Lee <ntoskrnl7@gmail.com>
2021-12-02 15:53:59 +0900

    Update submodules. Fix a ext::path::join functions.

--- test/path.cpp
+++ test/path.cpp
@@ -7,6 +7,34 @@ TEST(path_test, test) {
 #if defined(_WIN32)
   std::string r = ext::path::join("c:", "dir");
   EXPECT_STREQ(r.c_str(), "c:\\dir");
+
+  r = ext::path::dirname("c:\\dir\\test\\test");
+  EXPECT_STREQ(r.c_str(), "c:\\dir\\test");
+  
+  r = ext::path::basename("c:\\dir\\test\\test\\abc");
+  EXPECT_STREQ(r.c_str(), "abc");
+
+  r = ext::path::join("c:", "dir", ".test\\test", "..\\abc");
+  EXPECT_STREQ(r.c_str(), "c:\\dir\\.test\\abc");
+
+  r = ext::path::join("c:", "dir", "test\\te.st", ".\\abc");
+  EXPECT_STREQ(r.c_str(), "c:\\dir\\test\\te.st\\abc");
+
+  r = ext::path::join("c:", "dir", "test1\\test2", "..\\..\\..\\abc");
+  EXPECT_STREQ(r.c_str(), "c:\\abc");
+
+  r = ext::path::join("c:", "dir", "test1\\test2", "..\\.\\..\\abc");
+  EXPECT_STREQ(r.c_str(), "c:\\dir\\abc");
+
+  r = ext::path::join("c:", "dir", "test\\test", "..\\..\\..\\..\\abc");
+  EXPECT_STREQ(r.c_str(), "");
+
+  std::wstring ws = ext::path::join(L"c:", L"dir");
+  ws = ext::path::join(L"c:", L"dir", L"test\\te.st", L".\\abc");
+  EXPECT_STREQ(ws.c_str(), L"c:\\dir\\test\\te.st\\abc");
+
+  ws = ext::path::join(L"c:", L"dir", L"test1\\test2", L"..\\..\\..\\abc");
+  EXPECT_STREQ(ws.c_str(), L"c:\\abc");
 #else
   std::string r = ext::path::join("/home", "dir");
   EXPECT_STREQ(r.c_str(), "/home/dir");
